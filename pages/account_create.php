1700b811fba69f721af792b3aa76a3ac2c8f062e
seidaiegoshi <egoshi.seidai@gmail.com>
2022-12-06 17:50:19 +0900

    account登録機能追加

---
+++ pages/account_create.php
@@ -0,0 +1,50 @@
+<?php
+include('./functions/db.php');
+
+if (
+  !isset($_POST['username']) || $_POST['username'] == '' ||
+  !isset($_POST['password']) || $_POST['password'] == ''
+) {
+  exit('paramError');
+}
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+$pdo = connect_to_db();
+
+// 同じ名前のユーザーがいるかチェック
+$sql = 'SELECT COUNT(*) FROM users WHERE username=:username';
+
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':username', $username, PDO::PARAM_STR);
+
+try {
+  $status = $stmt->execute();
+} catch (PDOException $e) {
+  echo json_encode(["sql error" => "{$e->getMessage()}"]);
+  exit();
+}
+
+if ($stmt->fetchColumn() > 0) {
+  echo '<p>すでに登録されているユーザです．</p>';
+  echo '<a href="todo_login.php">login</a>';
+  exit();
+}
+
+// 同じ名前のユーザーがいなかったら登録
+$sql = 'INSERT INTO users(user_id, username, password) VALUES(NULL, :username, :password)';
+
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':username', $username, PDO::PARAM_STR);
+$stmt->bindValue(':password', $password, PDO::PARAM_STR);
+
+try {
+  $status = $stmt->execute();
+} catch (PDOException $e) {
+  echo json_encode(["sql error" => "{$e->getMessage()}"]);
+  exit();
+}
+
+header("Location:./user_top.php");
+exit();
