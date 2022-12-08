13f6d7113ab4c1eaec4f44efd95919c2a3abac61
seidaiegoshi <egoshi.seidai@gmail.com>
2022-12-09 01:20:07 +0900

    メンバー登録時にユーザーを判別

--- pages/team/team_member_add.php
+++ pages/team/team_member_add.php
@@ -4,7 +4,6 @@ include('./../functions/db.php');
 include('./../functions/is_login.php');
 check_session_id();
 
-var_dump($_POST);
 // var_dump($_POST);
 
 // 値がちゃんとあるかチェック。
@@ -23,7 +22,48 @@ $username = $_POST["username"];
 //DB接続
 $pdo = connect_to_db();
 
-// SQL作成&実行 チームを作成する。
+// すでにチームメンバーかどうか
+$sql = 'SELECT COUNT(*) FROM team_members WHERE username=:username AND team_id=:team_id';
+
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':username', $username, PDO::PARAM_STR);
+$stmt->bindValue(':team_id', $team_id, PDO::PARAM_STR);
+
+try {
+  $status = $stmt->execute();
+} catch (PDOException $e) {
+  echo json_encode(["sql error" => "{$e->getMessage()}"]);
+  exit();
+}
+
+if ($stmt->fetchColumn() > 0) {
+  echo '<p>すでに登録されているユーザです．</p>';
+  echo "<div><a href='./team_member.php?team_id={$team_id}'>メンバー編集ページへ</a></div>";
+  exit();
+}
+
+// そのユーザー存在してるかどうか
+$sql = 'SELECT COUNT(*) FROM users WHERE username=:username';
+
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':username', $username, PDO::PARAM_STR);
+
+try {
+  $status = $stmt->execute();
+} catch (PDOException $e) {
+  echo json_encode(["sql error" => "{$e->getMessage()}"]);
+  exit();
+}
+
+if ($stmt->fetchColumn() == 0) {
+  echo '<p>' . $username . 'は存在しないユーザーです。</p>';
+  echo "<div><a href='./team_member.php?team_id={$team_id}'>メンバー編集ページへ</a></div>";
+  exit();
+}
+
+
+
+// SQL作成&実行
 $sql = 'INSERT INTO team_members (id, team_id, username) VALUES (NULL, :team_id, :username);';
 
 $stmt = $pdo->prepare($sql);
