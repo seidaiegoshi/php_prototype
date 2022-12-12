11af7bd6fde98ff357ebce96597f1daa250488de
seidaiegoshi <egoshi.seidai@gmail.com>
2022-12-13 08:55:39 +0900

    プロフィールでお気に入りしているのプロジェクトを表示

--- pages/profile/manage_projects.php
+++ pages/profile/manage_projects.php
@@ -92,6 +92,88 @@ foreach ($result as $key => $record) {
   ";
 }
 
+
+// お気に入りに登録しているプロジェクトを表示
+// SQL作成&実行
+$sql = "SELECT * FROM projects
+INNER JOIN(
+SELECT project_id AS like_project_id,user_id 
+FROM project_like
+   WHERE user_id=:user_id
+) AS result
+ON projects.project_id = result.like_project_id
+";
+
+$stmt = $pdo->prepare($sql);
+
+// バインド変数を設定
+$stmt->bindValue(':user_id', $_SESSION["user_id"], PDO::PARAM_STR);
+
+// SQL実行（実行に失敗すると `sql error ...` が出力される）
+try {
+	$status = $stmt->execute();
+	// var_dump($status);
+} catch (PDOException $e) {
+	echo json_encode(["sql error" => "{$e->getMessage()}"]);
+	exit();
+}
+
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$favorite_projects = "";
+
+foreach ($result as $key => $record) {
+	$favorite_projects .= "
+  <div class='magazine'>
+	<a class='project' href='./../project/project_detail.php?project_id={$record["project_id"]}'>
+		<div class='image'>";
+	if ($record["image_url"] !== NULL) {
+		$favorite_projects .= "	
+			<img src='./../..{$record["image_url"]}'>";
+	} else {
+		$favorite_projects .= "	
+			<div class='no_image'>
+			<p>no image</p>	
+			</div>
+		";
+	}
+	$favorite_projects .= "
+		</div>
+		<div class='article'>
+			<div class='title'>
+				{$record["title"]}
+			</div>
+			<div class='content'>
+				{$record["content"]}
+			</div>
+
+			<div class='updated_at'>
+				{$record["updated_at"]}
+			</div>
+		</div>
+		</a>	
+
+		<div class='edit'>
+			<div>
+				<div class='project_edit'>
+				<form action='./../project/project_edit.php' method='POST'>
+				<input type='hidden' name='project_id' value='{$record["project_id"]}'>
+				<button>
+				EDIT
+				</button>
+				</form>
+				</div>
+				<div class='project_delete'>
+				<a href='./../project/project_delete.php?project_id={$record["project_id"]}'>
+					delete
+					</a>
+				</div>
+			</div>
+		</div>
+		</div>
+  ";
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -143,6 +225,10 @@ foreach ($result as $key => $record) {
 	<section class="search">
 		<h1>自分が携わっている商品</h1>
 		<?= $project_abstract_html_element ?>
+	</section>
+	<section class="favorite">
+		<h1>お気に入りのプロジェクト</h1>
+		<?= $favorite_projects ?>
 	</section>
 	<!-- <a href="">プロフィール</a> -->
 	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
